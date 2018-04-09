7c57cb326104db2a35eb580ea68de7a00e89bbec
ruslan.nafisovich@gmail.com <ruslan.nafisovich@gmail.com>
2018-04-09 14:15:02 +0300

    Реализованы методы для получения токена и обновления токена

---
+++ TokenAuthentication/Controllers/ResourceController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace TokenAuthentication.Controllers
+{
+    [Route("api")]
+    public class ResourceController : Controller
+    {
+        // POST api
+        [Authorize(Roles = "user")]
+        [HttpPost]
+        public IEnumerable<string> Post()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
---
+++ TokenAuthentication/Resources/AuthorizationTokenResource.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TokenAuthentication.Resource
+{
+    /// <summary>
+    /// Модель авторизованного пользователя
+    /// </summary>
+    public class AuthorizationTokenResource
+    {
+        /// <summary>
+        /// Токен для доступа к ресурсам
+        /// </summary>
+        public string AccessToken { get; set; }
+
+        /// <summary>
+        /// Токен для обновления токена доступа
+        /// </summary>
+        public string RefreshToken { get; set; }
+
+        /// <summary>
+        /// Время существования AccessToken'a
+        /// </summary>
+        public TimeSpan ExpiresIn { get; set; }
+    }
+}
